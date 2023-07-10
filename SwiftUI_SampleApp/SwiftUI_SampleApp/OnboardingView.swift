529dbfb57dd0e062f4b82b9ff32acb720fc795c3
전지훈 <junji636@gmail.com>
2023-07-10 23:50:34 +0900

    프로젝트 초기 설정

---
+++ SwiftUI_SampleApp/SwiftUI_SampleApp/OnboardingView.swift
@@ -0,0 +1,25 @@
+//
+//  OnboardingView.swift
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct OnboardingView: View {
+    var body: some View {
+        RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30)
+            .background(
+                Image("Spline")
+                    .blur(radius: 50)
+                    .offset(x: 200, y: 100)
+            )
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
---
+++ SwiftUI_SampleApp/SwiftUI_SampleApp/SwiftUI_SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SampleAppApp.swift
+//  SwiftUI_SampleApp
+//
+//  Created by 전지훈 on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
