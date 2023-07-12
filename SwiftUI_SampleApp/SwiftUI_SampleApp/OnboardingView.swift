397fe10c646e16ae580f004a3af512dc39a53466
전지훈 <junji636@gmail.com>
2023-07-12 23:18:38 +0900

    button action

--- SwiftUI_SampleApp/SwiftUI_SampleApp/OnboardingView.swift
+++ SwiftUI_SampleApp/SwiftUI_SampleApp/OnboardingView.swift
@@ -6,15 +6,36 @@ import SwiftUI
 import RiveRuntime
 
 struct OnboardingView: View {
+    let button = RiveViewModel(fileName: "button")
     var body: some View {
-        RiveViewModel(fileName: "shapes").view()
-            .ignoresSafeArea()
-            .blur(radius: 30)
-            .background(
-                Image("Spline")
-                    .blur(radius: 50)
-                    .offset(x: 200, y: 100)
-            )
+        ZStack {
+            RiveViewModel(fileName: "shapes").view()
+                .ignoresSafeArea()
+                .blur(radius: 30)
+                .background(
+                    Image("Spline")
+                        .blur(radius: 50)
+                        .offset(x: 200, y: 100)
+                )
+            
+            button.view()
+                .frame(width: 236, height: 64)
+                .overlay(
+                    Label("Start the course", systemImage: "arrow.forward")
+                        .offset(x: 4, y: 4)
+                        .font(.headline)
+                )
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+                .onTapGesture {
+                    try? button.play(animationName: "active")
+                }
+        }
     }
 }
 
