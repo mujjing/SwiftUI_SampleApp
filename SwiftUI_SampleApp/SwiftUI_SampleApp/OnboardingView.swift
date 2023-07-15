2f93ca7148bc2e3d147457e27483f845993e79f6
전지훈 <junji636@gmail.com>
2023-07-15 23:23:05 +0900

    modal transition작업

--- SwiftUI_SampleApp/SwiftUI_SampleApp/OnboardingView.swift
+++ SwiftUI_SampleApp/SwiftUI_SampleApp/OnboardingView.swift
@@ -7,45 +7,81 @@ import RiveRuntime
 
 struct OnboardingView: View {
     let button = RiveViewModel(fileName: "button")
+    @State var showModal = false
+
     var body: some View {
         ZStack {
             background
+            content
+                .offset(y: showModal ? -50 : 0)
             
-            VStack(alignment: .leading, spacing: 16) {
-                Text("Learn Design & Code")
-                    .font(.custom("Poppins-Bold", size: 60, relativeTo: .largeTitle))
-                    .frame(width: 260, alignment: .leading)
-                Text("Don't Skip Design")
-                    .customFont(textStyle: .body)
-                    .opacity(0.7)
-                
-                Spacer()
-                
-                button.view()
-                    .frame(width: 236, height: 64)
+            Color("Shadow")
+                .opacity(showModal ? 0.4 : 0)
+                .ignoresSafeArea()
+            
+            if showModal {
+                SignInView()
+                    .transition(.move(edge: .top).combined(with: .opacity))
                     .overlay(
-                        Label("Start the course", systemImage: "arrow.forward")
-                            .offset(x: 4, y: 4)
-                            .font(.headline)
+                        Button{
+                            withAnimation(.spring()) {
+                                showModal = false
+                            }
+                        } label: {
+                            Image(systemName: "xmark")
+                                .frame(width: 36, height: 36)
+                                .foregroundColor(.black)
+                                .background(.white)
+                                .mask(Circle())
+                                .shadow(color: Color("Shadow").opacity(0.3) , radius: 5, x: 0, y: 3)
+                        }
+                        .frame(maxHeight: .infinity, alignment: .bottom)
                     )
-                    .background(
-                        Color.black
-                            .cornerRadius(30)
-                            .blur(radius: 10)
-                            .opacity(0.3)
-                            .offset(y: 10)
-                    )
-                    .onTapGesture {
-                        try? button.play(animationName: "active")
-                    }
-                
-                Text("Purchase includes access to 30+ courses")
-                    .font(.footnote)
-                    .opacity(0.7)
+                    .zIndex(1)
             }
-            .padding(40)
-            .padding(.top, 40)
         }
+    }
+    
+    private var content: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Learn Design & Code")
+                .font(.custom("Poppins-Bold", size: 60, relativeTo: .largeTitle))
+                .frame(width: 260, alignment: .leading)
+            Text("Don't Skip Design")
+                .customFont(textStyle: .body)
+                .opacity(0.7)
+            
+            Spacer()
+            
+            button.view()
+                .frame(width: 236, height: 64)
+                .overlay(
+                    Label("Start the course", systemImage: "arrow.forward")
+                        .offset(x: 4, y: 4)
+                        .font(.headline)
+                )
+                .background(
+                    Color.black
+                        .cornerRadius(30)
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y: 10)
+                )
+                .onTapGesture {
+                    try? button.play(animationName: "active")
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+                        withAnimation(.spring()) {
+                            showModal = true
+                        }
+                    }
+                }
+            
+            Text("Purchase includes access to 30+ courses")
+                .font(.footnote)
+                .opacity(0.7)
+        }
+        .padding(40)
+        .padding(.top, 40)
     }
     
     private var background: some View {
