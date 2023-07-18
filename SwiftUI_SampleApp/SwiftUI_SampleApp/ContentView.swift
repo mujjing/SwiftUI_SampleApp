ac6e2803ad97da45ffcfb5a74389df82af720ed4
전지훈 <junji636@gmail.com>
2023-07-18 22:41:53 +0900

    HCard, HomeView 추가

--- SwiftUI_SampleApp/SwiftUI_SampleApp/ContentView.swift
+++ SwiftUI_SampleApp/SwiftUI_SampleApp/ContentView.swift
@@ -11,18 +11,28 @@ struct ContentView: View {
     let button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
     var body: some View {
         ZStack {
-            switch selectedTab {
-            case .chat:
-                OnboardingView()
-            case .search:
-                Text("Search")
-            case .timer:
-                Text("Timer")
-            case .bell:
-                Text("Bell")
-            case .user:
-                Text("User")
+            Group {
+                switch selectedTab {
+                    case .chat:
+                        HomeView()
+                    case .search:
+                        Text("Search")
+                    case .timer:
+                        Text("Timer")
+                    case .bell:
+                        Text("Bell")
+                    case .user:
+                        Text("User")
+                }
             }
+            .safeAreaInset(edge: .bottom) {
+                Color.clear.frame(height: 80)
+            }
+            .safeAreaInset(edge: .top) {
+                Color.clear.frame(height: 104)
+            }
+            .ignoresSafeArea()
+
             
             button.view()
                 .frame(width: 44, height: 44)
---
+++ SwiftUI_SampleApp/SwiftUI_SampleApp/Views/HomeView.swift
@@ -0,0 +1,52 @@
+//
+//  HomeView.swift
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color("Background").ignoresSafeArea()
+            
+            ScrollView {
+                content
+            }
+        }
+    }
+    
+    var content: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("Courses")
+                .customFont(textStyle: .largeTitle)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 20)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(courses) { course in
+                        VCard(course: course)
+                    }
+                }
+                .padding(20)
+                .padding(.bottom, 10)
+            }
+            Text("Recent")
+                .customFont(textStyle: .title3)
+                .padding(.horizontal, 20)
+            
+            VStack(spacing: 20) {
+                ForEach(courseSections) { section in
+                    HCard(section: section)
+                }
+            }
+            .padding(20)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
