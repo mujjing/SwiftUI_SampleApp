ca5451502e5af63d38e7f377fb9f90c1b97830a9
전지훈 <junji636@gmail.com>
2023-07-21 01:47:44 +0900

    main view animation 추가

--- SwiftUI_SampleApp/SwiftUI_SampleApp/Views/OnboardingView.swift
+++ SwiftUI_SampleApp/SwiftUI_SampleApp/Views/OnboardingView.swift
@@ -8,7 +8,7 @@ import RiveRuntime
 struct OnboardingView: View {
     let button = RiveViewModel(fileName: "button")
     @State var showModal = false
-
+    @Binding var show: Bool
     var body: some View {
         ZStack {
             background
@@ -39,6 +39,23 @@ struct OnboardingView: View {
                     )
                     .zIndex(1)
             }
+            
+            Button {
+                withAnimation {
+                    show = false
+                }
+            } label: {
+                Image(systemName: "xmark")
+                    .frame(width: 36, height: 36)
+                    .background(.black)
+                    .foregroundColor(.white)
+                    .mask(Circle())
+                    .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 10)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+            .padding(20)
+            .offset(y: showModal ? -200 : 80)
+
         }
     }
     
@@ -98,6 +115,6 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(show: .constant(false))
     }
 }
